a4120c5b4705ed7cc650f14028cf87d678a0931c
Ewan <ewan.hamon@gmail.com>
2024-10-25 10:02:07 +0200

    feat: add the css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // Enable dark mode
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
